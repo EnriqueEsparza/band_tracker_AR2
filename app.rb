d430160856a89898925d62d3d2285a467130b52c
paul morin & enrique esparza <student@epicodus.com>
2015-06-18 11:39:29 -0700

    have an index page listing band and venues

--- app.rb
+++ app.rb
@@ -1,3 +1,19 @@
 require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @bands = Band.all()
+  @venues = Venue.all()
+  erb(:index)
+end
+
+get("/bands") do
+  @bands = Band.all()
+  erb(:bands)
+end
+
+get("/venues") do
+  @venues = Venue.all()
+  erb(:venues)
+end
