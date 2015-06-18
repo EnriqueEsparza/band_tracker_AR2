f206c98e9f95e07a0c904f92c19c8c918893eae4
paul morin & enrique esparza <student@epicodus.com>
2015-06-18 15:01:58 -0700

    allow for adding/listing bands and venues, assaigning bands to venues and vice versa. Will also allow to edit and delete bands.

--- app.rb
+++ app.rb
@@ -33,3 +33,56 @@ post("/venues") do
   @venues = Venue.all()
   erb(:venues)
 end
+
+get("/bands/:id") do
+  @band = Band.find(params.fetch("id").to_i())
+  @venues = Venue.all()
+  erb(:band_info)
+end
+
+get("/venues/:id") do
+  @venue = Venue.find(params.fetch("id").to_i())
+  @bands = Band.all()
+  erb(:venue_info)
+end
+
+patch("/bands/:id") do
+  band_id = params.fetch("id").to_i()
+
+  @band = Band.find(band_id)
+  venue_ids = params.fetch("venue_ids")
+  @band.update({:venue_ids => venue_ids})
+  @venues = Venue.all()
+  erb(:band_info)
+end
+
+delete("/bands/:id") do
+  @band = Band.find(params.fetch("id").to_i())
+  @band.delete()
+  @bands = Band.all()
+  redirect('/')
+end
+
+patch("/venues/:id") do
+  venue_id = params.fetch("id").to_i()
+  @venue = Venue.find(venue_id)
+  band_ids = params.fetch("band_ids")
+  @venue.update({:band_ids => band_ids})
+  @bands = Band.all()
+  erb(:venue_info)
+end
+
+get("/bands/:id/edit") do
+  @band = Band.find(params.fetch("id").to_i())
+  erb(:band_name)
+end
+
+patch("/bands/:id/edit") do
+
+    name = params.fetch("name")
+    band_id = params.fetch("id").to_i()
+    @band = Band.find(band_id)
+    @band.update({:name => name})
+    @venues = Venue.all()
+    redirect("/")
+  end
